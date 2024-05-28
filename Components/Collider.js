8bc4b6d8fe35df87f103ba47caf1ab106a33f29f
DiegoArceLarios <112732312+DiegoArceLarios@users.noreply.github.com>
2024-05-29 01:54:19 +0300

    Add files via upload

---
+++ Components/Collider.js
@@ -0,0 +1,28 @@
+AFRAME.registerComponent('flying-birds', {
+    schema:{
+
+    },init: function(){
+        for(var i = 1; i<=20; i++){
+            var id = `hurdle${i}`
+
+            var px = Math.floor(Math.random()*3000 + (-1000))
+            var py = Math.floor(Math.random()*2 + (-1))
+            var pz = Math.floor(Math.random()*3000 + (-1000))
+            var position = {x: px, y : py, z : pz}
+
+            this.pajaritos(id, position)
+        }
+    },pajaritos: function (id, position){
+        //crear a los pajaros
+        var terrainEl = document.querySelector("#terrenoModelo")
+
+        var birdEl = document.createElement('a-entity')
+        birdEl.setAttribute('id', id)
+        birdEl.setAttribute('position', position)  
+        birdEl.setAttribute('scale', {x: 500, y: 500, z: 500})      
+        birdEl.setAttribute('gltf-model', './assets/models/flying_bird/scene.gltf')
+        birdEl.setAttribute('animation-mixer', {})
+
+        terrainEl.appendChild(birdEl)
+    }
+})
---
+++ Components/Target.js
@@ -0,0 +1,26 @@
+AFRAME.registerComponent('target-ring', {
+    schema:{
+        
+    },init: function(){
+        for(var i = 1 ; i <= 20; i++){
+            //id
+            var id = `ring${i}`
+            //variables de position
+            var px = (Math.random()*3000 + (-1000))
+            var py = (Math.random()*2 + (-1))
+            var pz = (Math.random()*3000 + (-1000))
+            var position = {x: px, y : py, z : pz}
+
+            this.createRings(id, position)
+        }
+    },createRings: function(id, position){
+        var terrainEl = document.querySelector('#terrenoModelo')
+        var ringEL = document.createElement('a-entity')
+        ringEL.setAttribute('id', id)
+        ringEL.setAttribute('position', position)
+        ringEL.setAttribute("material", "color", "#ff9100")
+        ringEL.setAttribute("geometry", {primitive: 'torus', radius: 8})
+
+        terrainEl.appendChild(ringEL)
+    }
+})
